23c0c15512b88141d85e760c07e03963c6950de0
Sharmin Sultana <sharmin9518@gmail.com>
2024-02-14 14:14:01 +0600

    explore-react-concepts

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Todo from './Todo'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      {/* <Person></Person>
+      <PersonR></PersonR>
+      <Student></Student>
+      <Device name="Laptop" price="100000"></Device>
+      <Device name="Mobile" price="1700"></Device>
+      <Device name="watch" price="250"></Device> */}
+
+    
+    </>
+  )
+}
+
+// function Person(){
+//   return(
+//     <h3>My name is Sharmin Sultana</h3>
+//   )
+// }
+
+// function PersonR(){
+//   const personStyle ={
+//     margin:'20px',
+//     paddingn:'25px',
+//     border:'2px solid purple',
+//     borderRadius:'10px'
+//   }
+//   return(
+//     <>
+//     <div style={personStyle}>
+//       <h4>About myself</h4>
+//       <h5>I'm Sharmin Sultana.I live in kapasia</h5>
+//       <h6>i completed cse from city University</h6>
+//     </div>
+//     </>
+//   )
+// }
+
+// function Student(){
+//     const age = 27;
+//     const person = {name:'karim', age:12
+//     }
+//   return(
+//     <>
+//       <div className='student'>
+//       <h4>my name is sharmin age:{age}</h4>
+//      <h6>I am {person.name} with age {person.age}</h6>
+//       </div>
+//     </>
+   
+//   )
+// }
+
+// function Device(props){
+//   console.log(props)
+//   return <h5>This is a device:{props.name} Price:{props.price}</h5>
+// }
+export default App
